01c610597edb43308ab14e8640cd8bcdc3e68815
HerculesGit <hercules.silva@dce.ufpb.br>
2018-08-13 09:27:52 -0300

    criação das funções de  subtração e porcentagem

--- calculadora/calc.py
+++ calculadora/calc.py
@@ -1,4 +1,12 @@
 def soma(x,y):
 	return x+y
+
 def divisao(x, y):
 	return x//y
+
+def subtracao(x,y):
+	return x-y
+
+def soma_porcentagem(valor,percent):
+	'inteiro- valor\n inteito- porcento'
+	return (valor * (percent))/100
